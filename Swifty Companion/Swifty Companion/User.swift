97ea2d562441a82a23cf686cceda1a21521db6b1
tony mack <tonymack@tonys-Mac.local>
2017-10-17 10:33:11 +0200

    need to clean

--- Swifty Companion/Swifty Companion/User.swift
+++ Swifty Companion/Swifty Companion/User.swift
@@ -6,46 +6,50 @@
 //  Copyright © 2017 janhoon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SwiftyJSON
 
 struct User {
-    let ID: int
+    let ID: Double
     let email: String
     let login: String
     let firstName: String
     let lastName: String
     let url: URL?
     let location: String
-    let profilePicture: Data
-    let mobileNumber: Integer
-    let wallet: Integer
+    let profilePicture: URL?
+    let mobileNumber: Double
+    let wallet: Double
     
-    let profileLevel: Integer           //todo
-    let Cursus: String = "WeThinkCode_"
+    //let profileLevel: Double           //todo
+    //let Cursus: String = "WeThinkCode_"
     
 
-    let correctionPoints: Integer
-    let skills: [String:Integer]        // might need its own struct
-    let projects: [String]              // might need its own struct
+    //let correctionPoints: Double
+    //let skills: [String:Double]        // might need its own struct
+    //let projects: [String]              // might need its own struct
     
-    init (data: NSDictionary?) {
+    init? (data: NSDictionary?) {
         guard
-            let ID = data?.value(forKeyPath: IntraKey.ID) as? Integer,
+            let ID = data?.value(forKeyPath: IntraKey.ID) as? Double,
             let login = data?.value(forKeyPath: IntraKey.login) as? String,
             let email = data?.value(forKeyPath: IntraKey.email) as? String
         else {
             return nil
             }
         
+     //   let urlString = data?.value(forKeyPath: IntraKey.url) as? String ?? ""
+       // self.profileImageURL = (urlString.characters.count > 0) ? URL(string: urlString) : nil
+        
         let firstName = data?.value(forKeyPath: IntraKey.firstName) as? String ?? ""
         let lastName = data?.value(forKeyPath: IntraKey.lastName) as? String ?? ""
-        let userurl = data?.value(forKeyPath: IntraKey.url) as? URL ?? ""
+        let userurl = data?.value(forKeyPath: IntraKey.url) as? String ?? ""
         let url = (userurl.characters.count > 0) ? URL(string: userurl) : nil
         let location = data?.value(forKeyPath: IntraKey.location) as? String ?? ""
         let urlString = data?.value(forKeyPath: IntraKey.profilePicture) as? String ?? ""
         let profilePicture = (urlString.characters.count > 0) ? URL(string: urlString) : nil
-        let mobilePhone = data?.value(forKeyPath: IntraKey.mobileNumber) as? Integer ?? 0
-        let wallet =  data?.value(forKeyPath: IntraKey.wallet) as? Integer ?? 0
+        let mobilePhone = data?.value(forKeyPath: IntraKey.mobileNumber) as? Double ?? 0
+        let wallet =  data?.value(forKeyPath: IntraKey.wallet) as? Double ?? 0
         
         self.ID = ID
         self.email = email
@@ -69,13 +73,13 @@ struct User {
         static let url = "url"
         static let email = "email"
         static let profilePicture = "image_url"
-        static let profileLevel = ""                ///todo
-        static let Cursus: String = "WeThinkCode_"
+        //static let profileLevel = ""                ///todo
+        //static let Cursus: String = "WeThinkCode_"
         static let mobileNumber = "phone"
         static let location = "location"
         static let wallet = "wallet"
-        static let correctionPoints = "correction_points"
-        static let skills: [String:Integer]        // might need its own struct
-        static let projects: [String]
+        //static let correctionPoints = "correction_points"
+        //static let skills: [String:Double]        // might need its own struct
+        //static let projects: [String]
     }
 }
